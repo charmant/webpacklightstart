1a15509e182571c2a7cdfd9bb904f163b471cf9b
sergeipotenko <sergei.potenko@gmail.com>
2019-08-04 19:48:40 +0300

    init

---
+++ build/webpack.build.conf.js
@@ -0,0 +1,23 @@
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.conf')
+const ImageminPlugin = require('imagemin-webpack-plugin').default
+
+const buildWebpackConfig = merge(baseWebpackConfig, {
+  // BUILD config
+  mode: 'production',
+  plugins: [
+    new ImageminPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      optipng: {
+        optimizationLevel: 3
+      },
+      jpegtran: {
+        progressive: true
+      }
+    })
+  ]
+})
+
+module.exports = new Promise((resolve, reject) => {
+  resolve(buildWebpackConfig)
+})
